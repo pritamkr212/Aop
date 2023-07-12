7554f26f1f1f9190c7a4b0cb7d55525989df48ca
EmPritam <pritamkr212@gmail.com>
2023-07-13 00:26:43 +0530

    Intialization

---
+++ src/main/java/com/SecurityAOP/AopSecurity/AopSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.SecurityAOP.AopSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SecurityAOP/AopSecurity/AopSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SecurityAOP.AopSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
