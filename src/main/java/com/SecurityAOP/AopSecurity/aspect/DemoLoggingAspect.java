8a14670c7c6931238287f758759176b358c1e9d8
pritam <pritam.kumar@ofbusiness.in>
2023-07-13 18:17:34 +0530

    PointCutExpression

--- src/main/java/com/SecurityAOP/AopSecurity/aspect/DemoLoggingAspect.java
+++ src/main/java/com/SecurityAOP/AopSecurity/aspect/DemoLoggingAspect.java
@@ -2,14 +2,26 @@ package com.SecurityAOP.AopSecurity.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class DemoLoggingAspect {
-    @Before("execution(* com.SecurityAOP.AopSecurity.dao.*.*(..))")
+
+    //Declaration of pointcut
+    @Pointcut("execution(* com.SecurityAOP.AopSecurity.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    //Applying pointcut declaration to advice
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice(){
         System.out.println("Check Before adding");
+    }
+
+    @Before("forDaoPackage()")
+    public void performanceAnalytics(){
+        System.out.println("Performance compromised");
     }
 }
